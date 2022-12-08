e5a10ea5d3c9071e5e7c77aa7fd02bc8a21045be
Smoke5643 <smoke5643@gmail.com>
2022-12-07 22:09:48 -0500

    ready for last step

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
 // for loop hour 9-17
-// determine past grey present red future green
-
-
 
 $(function blockColor() {
  
   var saveButton = $('.saveBtn');
-  var currentHour = dayjs().subtract(5, 'hour').hour();
+  var currentHour = dayjs().hour();
  
   $('.description').each(function() {
-    // $(this).parent().removeClass('past present future');
+    
     var divTime = parseInt($(this).parent().attr('id').replace("hour-", ""));
     if (divTime < currentHour){
       $(this).addClass('past');
@@ -27,12 +24,6 @@ $(function blockColor() {
     localStorage.setItem(textInput, time);
    
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
